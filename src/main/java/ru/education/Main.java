72aee321b6a49f0496cc40aa9543d09ba8cdaf4d
fadeciness <solodimov.eugene@yandex.ru>
2020-06-15 16:40:56 +0300

    Signed commit

--- src/main/java/ru/education/Main.java
+++ src/main/java/ru/education/Main.java
@@ -3,5 +3,7 @@ package ru.education;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Now The commit is signing");
     }
 }
